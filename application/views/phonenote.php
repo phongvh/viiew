31ae4d2fb26d62c4bcba1525b0a49bec5fc0a8b3
Phong <phongvh50ca@gmail.com>
2013-12-18 14:42:00 +0700

    phonenote form didn't work since we enabled csrf, this is fixed by using form_open()

--- application/views/phonenote.php
+++ application/views/phonenote.php
@@ -3,7 +3,8 @@
   	
   	<div class="block block-center" id="help-block">
 	    <h2>Enter your note here!</h2>
-	    <form class="" action="" method="post" id="form-enter-name">
+	    <?php echo form_open("",array('id'=>'form-enter-name')); ?>
+	    <!-- <form class="" action="" method="post" id="form-enter-name"> -->
 	    	<textarea name="note" placeholder="Note here" class="input-block-level" required></textarea>
 	    	<input type="submit" class="btn-primary btn" value="Save">
 	    </form>
