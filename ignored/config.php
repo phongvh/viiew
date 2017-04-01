dea470fbe44567da47ffd7d4d61737600e730a61
Phong <phongvh50ca@gmail.com>
2017-04-01 08:49:58 +0700

    Update config.php

--- ignored/config.php
+++ ignored/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://viiew';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://viiew';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
